a8b6ba0ddc253c4b27dedf4f2030be92de326fbe
Jan Pretzel <jan.pretzel@deepsource.de>
2013-12-18 12:54:38 +0100

    solve problem 4 - description

--- src/e004/Euler004.java
+++ src/e004/Euler004.java
@@ -2,6 +2,12 @@ package e004;
 
 import utility.MathHelper;
 
+/**
+ * A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is
+ * 9009 = 91 × 99.
+ *
+ * Find the largest palindrome made from the product of two 3-digit numbers.
+ */
 public class Euler004 {
     public static void main(String[] args) {
         System.out.println(new Euler004().run());
